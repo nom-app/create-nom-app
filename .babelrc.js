3799d1f7b6812711d0b668c274ec3f96c68615ac
Maxim Devoir <maximdevoir@hotmail.com>
2019-05-14 23:58:55 -0700

    Added babel transform runtime

--- .babelrc.js
+++ .babelrc.js
@@ -1,4 +1,9 @@
 module.exports = {
-  presets: ['@babel/preset-env'],
-  plugins: ['add-module-exports']
+  presets: [
+    ['@babel/preset-env']
+  ],
+  plugins: [
+    'add-module-exports',
+    '@babel/plugin-transform-runtime'
+  ]
 }
