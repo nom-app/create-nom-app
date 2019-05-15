fc34c6f3a8b011e387fa870b247ac95a4b22d89a
Maxim Devoir <maximdevoir@hotmail.com>
2019-05-15 13:14:09 -0700

    Added version validation for NPM
    We use v6.0.0 as it is almost a year old.

--- src/validators/validateNPMVersion.js
+++ src/validators/validateNPMVersion.js
@@ -1,12 +1,20 @@
+import chalk from 'chalk'
 import semver from 'semver'
 
-const minimumNPMVersion = '6.10.0'
+const minimumNPMVersion = '6.0.0'
 
-// TODO: Require minimum supported version gta 6.0.0
 function validateNPMVersion(version) {
-  console.log('validating npm', version)
+  console.log('Validating NPM version', version)
+
   if (!semver.gte(version, minimumNPMVersion)) {
-    console.warn('uh oh')
+    console.error(
+      chalk.red(
+        'Unable to create the project.'
+        + `Your version of NPM ${version} is unsupported.\n`
+        + `Create Nom App requires NPM ${minimumNPMVersion} or higher.\n`
+        + 'Please update NPM.'
+      )
+    )
 
     process.exit(1)
   }
