11caf7c48330bd0fb847a72a56ac221b26f25a99
Maxim Devoir <maximdevoir@hotmail.com>
2019-05-15 13:13:26 -0700

    Added version validation for Yarn
    We started off with a minimum yarn version of 1.8.0 as it is almost a year old at this point.

--- src/validators/validateYarnVersion.js
+++ src/validators/validateYarnVersion.js
@@ -1,8 +1,23 @@
+import chalk from 'chalk'
 import semver from 'semver'
 
-// TODO: Require minimum support version gte 1.8.0
+const minimumYarnVersion = '1.8.0'
+
 function validateYarnVersion(version) {
-  console.log('validating yarn', version)
+  console.log('Validating Yarn version', version)
+
+  if (!semver.gte(version, minimumYarnVersion)) {
+    console.error(
+      chalk.red(
+        'Unable to create the project.'
+        + `Your version of Yarn ${version} is unsupported.\n`
+        + `Create Nom App requires Yarn ${minimumYarnVersion} or higher.\n`
+        + 'Please update Yarn.'
+      )
+    )
+
+    process.exit(1)
+  }
 }
 
 export default validateYarnVersion
