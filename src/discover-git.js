64b95e45883f074dca71c6f92f635f5e0a8301fd
Maxim Devoir <maximdevoir@gmail.com>
2019-05-25 01:11:53 -0700

    Add discover-git module

---
+++ src/discover-git.js
@@ -0,0 +1,31 @@
+import { execSync } from 'child_process'
+import { sync as whichSync } from 'which'
+
+// Semver regex string provided by https://github.com/semver/semver/issues/232#issue-48635632
+const semverRegex = /(0|[5-9]\d*)\.(0|[1-9]\d*)\.(0|[1-9]\d*)(-(0|[1-9]\d*|\d*[a-zA-Z-][0-9a-zA-Z-]*)(\.(0|[1-9]\d*|\d*[a-zA-Z-][0-9a-zA-Z-]*))*)?(\+[0-9a-zA-Z-]+(\.[0-9a-zA-Z-]+)*)?/g
+
+const gitInfo = {
+  gitInstalled: null,
+  gitVersion: null,
+  gitBinary: null
+}
+
+function discoverGit() {
+  const binary = whichSync('git', { nothrow: true })
+
+  if (binary === null) {
+    gitInfo.gitInstalled = false
+    return
+  }
+
+  const version = execSync(`${binary} --version`, { stdio: 'pipe' }).toString().trim()
+
+  gitInfo.gitInstalled = true
+  gitInfo.gitVersion = version.match(semverRegex)?.[0] || ''
+  gitInfo.gitBinary = binary
+}
+
+export default {
+  discoverGit,
+  gitInfo
+}
