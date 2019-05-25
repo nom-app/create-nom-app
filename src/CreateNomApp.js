839bf1fc2f3c4dd0afbd5eefb038292058955e17
Maxim Devoir <maximdevoir@gmail.com>
2019-05-25 01:23:06 -0700

    Initial Create Nom App class

--- src/CreateNomApp.js
+++ src/CreateNomApp.js
@@ -1,6 +1,12 @@
+import { execSync } from 'child_process'
+import os from 'os'
+import path from 'path'
+import fs from 'fs-extra'
+
 const defaultOptions = {
   projectDirectory: null,
-  packageManagerBinary: null
+  packageManagerBinary: null,
+  gitBinary: null
 }
 
 class CreateNomApp {
@@ -10,8 +16,68 @@ class CreateNomApp {
     this.options = Object.assign({}, defaultOptions, options)
   }
 
+  /**
+   * Initializes a Git repo inside the project directory.
+   */
+  initGitRepo() {
+    if (typeof this.options.gitBinary !== 'string') {
+      console.log('Skipping Git repo creation. Could not find Git on the system.')
+      return
+    }
+
+    console.log('git binary path is', this.options.gitBinary)
+    execSync(`${this.options.gitBinary} init`, {
+      cwd: this.options.projectDirectory
+    })
+    console.log('git adding files')
+    execSync(`${this.options.gitBinary} add .`, {
+      cwd: this.options.projectDirectory
+    })
+    console.log('git initial commit')
+    execSync(`${this.options.gitBinary} commit -m "Initial commit from Create Nom App"`, {
+      cwd: this.options.projectDirectory
+    })
+  }
+
   create() {
     console.log(`Creating app ${this.projectName}`)
+    // console.log('Ensuring app directory')
+    // fs.ensureDirSync(this.options.projectDirectory)
+    console.log('Copying structure and files from template.')
+    const sourceTemplatePath = path.join(__dirname, 'templates', 'base')
+    console.log('from: ', sourceTemplatePath)
+    console.log('to: ', this.options.projectDirectory)
+    fs.copySync(sourceTemplatePath, this.options.projectDirectory)
+
+    // TODO: `nom-scripts` needs to point to the local development directory
+    // when in development mode because...
+
+    const basicPackageJson = {
+      name: this.projectName,
+      description: '',
+      version: '0.1.0',
+      main: 'lib/index.js',
+      private: true,
+      scripts: {
+        start: 'nom-scripts start',
+        test: 'nom-scripts test',
+        build: 'nom-scripts build',
+        eject: 'nom-scripts eject'
+      },
+      eslintConfig: {
+        extends: 'eslint-config-airbnb'
+      },
+      keywords: ['create-nom-app', 'boilerplate'],
+      dependencies: {
+        // TODO: add nom-scripts dependency
+      }
+    }
+
+    fs.writeJSONSync(path.join(this.options.projectDirectory, 'package.json'), basicPackageJson, {
+      spaces: 2,
+      EOL: os.EOL,
+      mode: '644'
+    })
   }
 }
 
