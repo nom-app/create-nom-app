17734ae7d7081b7a48ab1eaaecc19c86c8c20653
Maxim Devoir <maximdevoir@gmail.com>
2019-05-30 09:59:38 -0700

    Switch to static year

--- docs/website/siteConfig.js
+++ docs/website/siteConfig.js
@@ -72,7 +72,7 @@ const siteConfig = {
   */
 
   // This copyright info is used in /core/Footer.js and blog RSS/Atom feeds.
-  copyright: `Copyright © ${new Date().getFullYear()} Maxim Devoir`,
+  copyright: `Copyright © 2019 Maxim Devoir`,
 
   highlight: {
     // Highlight.js theme to use for syntax highlighting in code blocks.
