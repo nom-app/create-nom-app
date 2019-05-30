067289f67333d728b6e0fac297989527e7cbc02f
Maxim Devoir <maximdevoir@gmail.com>
2019-05-30 10:31:38 -0700

    Fix links

--- docs/website/core/Footer.js
+++ docs/website/core/Footer.js
@@ -37,14 +37,11 @@ class Footer extends React.Component {
           </a>
           <div>
             <h5>Docs</h5>
-            <a href={this.docUrl('doc1.html', this.props.language)}>
-              Getting Started (or other categories)
+            <a href={this.docUrl('getting-started')}>
+              Getting Started
             </a>
-            <a href={this.docUrl('doc2.html', this.props.language)}>
-              Guides (or other categories)
-            </a>
-            <a href={this.docUrl('doc3.html', this.props.language)}>
-              API Reference (or other categories)
+            <a href={this.docUrl('contributing-introduction')}>
+              Contributing
             </a>
           </div>
           <div>
