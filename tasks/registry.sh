935dc0420839fe7ef8ce63535e9c6876724f6956
Maxim Devoir <maximdevoir@gmail.com>
2019-07-07 13:13:13 -0700

    Fix [SC2006](https://github.com/koalaman/shellcheck/wiki/SC2006)

--- tasks/registry.sh
+++ tasks/registry.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 
 verdaccio_registry=http://localhost:4873
-original_npm_registry=`npm config get registry`
-original_yarn_registry=`yarn config get registry`
+original_npm_registry=$(npm config get registry)
+original_yarn_registry=$(yarn config get registry)
 
 function useVerdaccioRegistry () {
   _printRegistryInformation
