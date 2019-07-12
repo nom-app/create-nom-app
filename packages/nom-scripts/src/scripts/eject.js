0628f0933be76a4ecdb370cca9c947c64a4f4ead
Maxim Devoir <maximdevoir@gmail.com>
2019-07-12 11:38:18 -0700

    Simple eject message

---
+++ packages/nom-scripts/src/scripts/eject.js
@@ -0,0 +1,4 @@
+console.log('Eject is not available in your version of Create Nom App.')
+console.log('Check the status of eject here:')
+console.log('TODO:Link to an eject url')
+console.log('You may need to update your global create-nom-app package.')
