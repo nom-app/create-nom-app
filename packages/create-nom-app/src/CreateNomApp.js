2dae64aaf15bfec40419a75253759da4751eb21e
Maxim Devoir <maximdevoir@gmail.com>
2019-07-12 14:05:00 -0700

    Remove dead code

--- packages/create-nom-app/src/CreateNomApp.js
+++ packages/create-nom-app/src/CreateNomApp.js
@@ -1,8 +1,5 @@
-/* eslint-disable no-underscore-dangle */
-import { execSync } from 'child_process'
 import { sync as spawnSync } from 'cross-spawn'
 
-import os from 'os'
 import path from 'path'
 import fs from 'fs-extra'
 
@@ -23,56 +20,6 @@ class CreateNomApp {
 
   ensureProjectDir() {
     fs.ensureDirSync(this.options.projectDirectory)
-  }
-
-  /**
-   * Initializes a Git repo inside the project directory.
-   */
-  initGitRepo() {
-    if (typeof this.options.gitBinary !== 'string') {
-      console.log('Skipping Git repo creation. Could not find Git on the system.')
-      return
-    }
-
-    console.log('git binary path is', this.options.gitBinary)
-    execSync(`${this.options.gitBinary} init`, {
-      cwd: this.options.projectDirectory
-    })
-    console.log('git adding files')
-    execSync(`${this.options.gitBinary} add .`, {
-      cwd: this.options.projectDirectory
-    })
-    console.log('git initial commit')
-    execSync(`${this.options.gitBinary} commit -m "Initial commit from Create Nom App"`, {
-      cwd: this.options.projectDirectory
-    })
-  }
-
-  _writeBasePackage() {
-    const basePackage = {
-      name: this.projectName,
-      description: 'A boilerplate create-nom-app project.',
-      version: '0.1.0',
-      main: 'dist/main.js',
-      private: true,
-      scripts: {
-        start: 'nom-scripts start',
-        test: 'nom-scripts test',
-        build: 'nom-scripts build',
-        eject: 'nom-scripts eject'
-      },
-      eslintConfig: {
-        extends: 'eslint-config-airbnb'
-      },
-      keywords: ['create-nom-app', 'boilerplate'],
-      dependencies: {}
-    }
-
-    fs.writeJSONSync(path.join(this.options.projectDirectory, 'package.json'), basePackage, {
-      spaces: 2,
-      EOL: os.EOL,
-      mode: '644'
-    })
   }
 
   installPackages() {
@@ -104,7 +51,6 @@ class CreateNomApp {
   }
 
   handoff() {
-    console.log('start of handoff')
     const nomScriptsBin = path.resolve(this.options.projectDirectory,
       path.join(this.options.projectDirectory, 'node_modules', '.bin', 'nom-scripts'))
     const handoffOptions = JSON.stringify({
@@ -122,41 +68,6 @@ class CreateNomApp {
       console.log('Create Nom App failed during handoff to local create-nom-app project.')
       process.exit(handoffProc.status)
     }
-
-    console.log('end handoff')
-  }
-
-  create() {
-    console.log(`Creating app ${this.projectName}`)
-    // console.log('Ensuring app directory')
-    // fs.ensureDirSync(this.options.projectDirectory)
-    console.log('Copying structure and files from template.')
-    const sourceTemplatePath = path.join(__dirname, 'templates', 'base')
-    console.log('from: ', sourceTemplatePath)
-    console.log('to: ', this.options.projectDirectory)
-    fs.copySync(sourceTemplatePath, this.options.projectDirectory)
-
-    // Convert files to dotfiles, relative to project root directory.
-    const dotfiles = ['gitignore', ['npmignore']]
-
-    dotfiles.forEach((entry) => {
-      const dotfile = typeof entry === 'string' ? entry : path.join(...entry)
-      const nonDotfilePath = path.join(this.options.projectDirectory, dotfile)
-      const pathToDotfile = path.join(this.options.projectDirectory, `.${dotfile}`)
-      console.log('copying non-dotfile to ', pathToDotfile)
-      try {
-        if (fs.existsSync(nonDotfilePath)) {
-          fs.renameSync(nonDotfilePath, pathToDotfile)
-        }
-      } catch (err) {
-        console.error(err)
-      }
-    })
-
-    // Create a package.json
-    // TODO: `nom-scripts` needs to point to the local development directory
-    // when in development mode because...
-    this._writeBasePackage()
   }
 }
 
