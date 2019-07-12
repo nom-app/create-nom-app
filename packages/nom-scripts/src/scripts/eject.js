c222a50a5448d6468f93348049f91e6610de937e
Maxim Devoir <maximdevoir@gmail.com>
2019-07-12 14:05:41 -0700

    Improve message

--- packages/nom-scripts/src/scripts/eject.js
+++ packages/nom-scripts/src/scripts/eject.js
@@ -1,4 +1,4 @@
-console.log('Eject is not available in your version of Create Nom App.')
+console.log('Eject is not available in your version of nom-scripts.')
 console.log('Check the status of eject here:')
 console.log('TODO:Link to an eject url')
-console.log('You may need to update your global create-nom-app package.')
+console.log('You may need to update your version of nom-scripts.')
