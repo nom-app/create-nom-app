2b75e70db8421de62ddd9d6d644ab959c3c5dd03
Maxim Devoir <maximdevoir@gmail.com>
2019-07-17 10:29:33 -0700

    Update eject message

--- packages/nom-scripts/src/scripts/eject.js
+++ packages/nom-scripts/src/scripts/eject.js
@@ -1,4 +1,3 @@
 console.log('Eject is not available in your version of nom-scripts.')
-console.log('Check the status of eject here:')
-console.log('TODO:Link to an eject url')
-console.log('You may need to update your version of nom-scripts.')
+// console.log('TODO:Link to status of eject')
+console.log('\nYou may need to update your nom-scripts dependency.\n')
