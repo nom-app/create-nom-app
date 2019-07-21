f45bfe53f5eb5eee7159341676974a68ca6be5f5
Maxim Devoir <maximdevoir@gmail.com>
2019-07-21 15:16:19 -0700

    Update index

--- docs/website/pages/en/index.js
+++ docs/website/pages/en/index.js
@@ -64,9 +64,8 @@ class HomeSplash extends React.Component {
         <div className="inner">
           <ProjectTitle siteConfig={siteConfig} />
           <PromoSection>
-            <Button href="#try">Try It Out</Button>
-            <Button href={docUrl('doc1.html')}>Example Link</Button>
-            <Button href={docUrl('doc2.html')}>Example Link 2</Button>
+            <Button href={docUrl('getting-started')}>Visit the Docs</Button>
+            {/* <Button href={docUrl('doc1.html')}>Example Link</Button> */}
           </PromoSection>
         </div>
       </SplashContainer>
@@ -101,29 +100,13 @@ class Index extends React.Component {
       </div>
     );
 
-    const TryOut = () => (
-      <Block id="try">
-        {[
-          {
-            content:
-              'To make your landing page more attractive, use illustrations! Check out ' +
-              '[**unDraw**](https://undraw.co/) which provides you with customizable illustrations which are free to use. ' +
-              'The illustrations you see on this page are from unDraw.',
-            image: `${baseUrl}img/undraw_code_review.svg`,
-            imageAlign: 'left',
-            title: 'Wonderful SVG Illustrations',
-          },
-        ]}
-      </Block>
-    );
-
     const Description = () => (
       <Block background="dark">
         {[
           {
             content:
               'This is another description of how this project is useful',
-            image: `${baseUrl}img/undraw_note_list.svg`,
+            image: `https://user-images.githubusercontent.com/10104630/61597124-1a500380-abc1-11e9-9446-b382301bb9af.gif`,
             imageAlign: 'right',
             title: 'Description',
           },
@@ -197,12 +180,11 @@ class Index extends React.Component {
       <div>
         <HomeSplash siteConfig={siteConfig} language={language} />
         <div className="mainContainer">
-          <Features />
+          {/* <Features /> */}
           <FeatureCallout />
-          <LearnHow />
-          <TryOut />
+          {/* <LearnHow /> */}
           <Description />
-          <Showcase />
+          {/* <Showcase /> */}
         </div>
       </div>
     );
