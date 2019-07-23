c54ac208345551eccc131f515e2f6f611114d9cd
Maxim Devoir <maximdevoir@gmail.com>
2019-07-22 21:22:35 -0700

    Handle environment variables better

--- services/termr/render.js
+++ services/termr/render.js
@@ -1,33 +1,29 @@
 const cs = require('cross-spawn')
+const fs = require('fs-extra')
 const path = require('path')
 const isDir = require('is-directory')
 const glob = require('glob')
 
 const XVFB_SERVER_ARG = process.env.XVFB_SERVER_ARG || '-screen 0 1920x1080x24'
-let RAW_TERM_DIR = process.env.RAW_TERM_DIR
-let OUTPUT_DIR = process.env.OUTPUT_DIR
+let RAW_TERM_DIR = path.isAbsolute(process.env.RAW_TERM_DIR)
+  ? process.env.RAW_TERM_DIR
+  : path.resolve(process.env.RAW_TERM_DIR)
+let OUTPUT_DIR = path.isAbsolute(process.env.OUTPUT_DIR)
+  ? process.env.OUTPUT_DIR
+  : path.resolve(process.env.OUTPUT_DIR)
 
+fs.ensureDirSync(OUTPUT_DIR)
 
 if (typeof RAW_TERM_DIR !== 'string' || !isDir.sync(RAW_TERM_DIR)) {
-  console.error('RAW_TERM_DIR environment variable must point to a directory')
-  console.error('RAW_TERM_DIR currently is set to:', RAW_TERM_DIR)
+  console.error('RAW_TERM_DIR must point to a directory')
+  console.error('RAW_TERM_DIR currently is pointing to:', RAW_TERM_DIR)
   process.exit(1)
 }
 
 if (typeof OUTPUT_DIR !== 'string' || !isDir.sync(OUTPUT_DIR)) {
-  console.error('OUTPUT_DIR environment variable must point to a directory')
-  console.error('OUTPUT_DIR currently is set to:', OUTPUT_DIR)
+  console.error('OUTPUT_DIR must point to a directory')
+  console.error('OUTPUT_DIR currently is pointing to:', OUTPUT_DIR)
   process.exit(1)
-}
-
-if (!path.isAbsolute(RAW_TERM_DIR)) {
-  console.log('Converting RAW_TERM_DIR to absolute path, based on current working directory.')
-  RAW_TERM_DIR = path.resolve(RAW_TERM_DIR)
-}
-
-if (!path.isAbsolute(OUTPUT_DIR)) {
-  console.log('Converting OUTPUT_DIR to absolute path, based on current working directory.')
-  OUTPUT_DIR = path.resolve(OUTPUT_DIR)
 }
 
 const rawTerminalFiles = glob.sync(`${RAW_TERM_DIR}/*.yml`)
