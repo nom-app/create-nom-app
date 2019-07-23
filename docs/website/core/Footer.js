4c133d506ff9a3daa4b329ac8ad57cd5fbbdef03
Maxim Devoir <maximdevoir@gmail.com>
2019-07-22 21:21:22 -0700

    Add Project Chat link

--- docs/website/core/Footer.js
+++ docs/website/core/Footer.js
@@ -56,10 +56,10 @@ class Footer extends React.Component {
               Stack Overflow
             </a>
             <a
-              href="https://twitter.com/"
+              href="https://discord.gg/T3qZyb2"
               target="_blank"
               rel="noreferrer noopener">
-              Twitter
+              Project Chat
             </a>
           </div>
           <div>
