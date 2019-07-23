b2d1b0fe04d6faeeb97fab69d654b6954f2fdee6
Maxim Devoir <maximdevoir@gmail.com>
2019-07-23 13:10:39 -0700

    Link to GitHub project

--- docs/website/core/Footer.js
+++ docs/website/core/Footer.js
@@ -65,7 +65,7 @@ class Footer extends React.Component {
           <div>
             <h5>More</h5>
             {/* <a href={`${this.props.config.baseUrl}blog`}>Blog</a> */}
-            <a href="https://github.com/">GitHub</a>
+            <a href="https://github.com/MaximDevoir/create-nom-app">GitHub</a>
             <a
               className="github-button"
               href={this.props.config.repoUrl}
