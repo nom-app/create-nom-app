d7fdfd045d56ef50b97c0c805be0acb7670f592f
Maxim Devoir <maximdevoir@gmail.com>
2019-08-20 19:55:19 -0700

    Initial publish script

---
+++ tasks/publish.sh
@@ -0,0 +1,22 @@
+#!/bin/bash
+
+# Print each command and their arguments as they are executed
+set -x
+
+# Exit if a command returns a non-zero code
+set -e
+
+# Go to root of repository
+cd "$(dirname "$(dirname "${BASH_SOURCE[0]}")")"
+
+# Install packages
+yarn
+
+# Cleans build directory, code coverage results, and more
+./node_modules/.bin/lerna exec -- yarn clean
+
+# Build packages
+./node_modules/.bin/lerna exec -- yarn build
+
+# Publish packages
+./node_modules/.bin/lerna publish --npm-client=npm "$@"
