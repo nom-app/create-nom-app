15b78fab471885f41207450c9e384dfe4284afdb
Maxim Devoir <maximdevoir@gmail.com>
2019-08-20 20:07:03 -0700

    Check if repository is clean

--- tasks/publish.sh
+++ tasks/publish.sh
@@ -18,5 +18,10 @@ yarn
 # Build packages
 ./node_modules/.bin/lerna exec -- yarn build
 
+if [ -n "$(git status --porcelain)" ]; then
+  echo "There are uncommitted changes in the repository." >&2
+  exit 1
+fi
+
 # Publish packages
 ./node_modules/.bin/lerna publish --npm-client=npm "$@"
