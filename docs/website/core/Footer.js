2ed2f5a22f00af28cbadf383bac4de0545cdb7bf
Maxim Devoir <MaximDevoir@users.noreply.github.com>
2019-09-30 19:51:51 -0700

    Update Footer.js

--- docs/website/core/Footer.js
+++ docs/website/core/Footer.js
@@ -78,7 +78,6 @@ class Footer extends React.Component {
             </a>
           </div>
         </section>
-
         <section className="copyright">{this.props.config.copyright}</section>
       </footer>
     );
