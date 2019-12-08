17ee991a01eb2ec632d9d30854df64c38e424269
Maxim Devoir <maximdevoir@gmail.com>
2019-12-08 06:48:47 -0800

    feat: add TypeScript linting with Prettier

--- .eslintrc.js
+++ .eslintrc.js
@@ -4,8 +4,17 @@ module.exports = {
   "parserOptions": {
     "project": "./tsconfig.json",
     "sourceType": "module",
-    "ecmaVersion": 2020
+    "ecmaVersion": 2018
   },
   "plugins": ["@typescript-eslint"],
-  "extends": ["plugin:@typescript-eslint/recommended"]
+  "extends": [
+    "eslint:recommended", // Uses the set of rules recommended by the ESLint Team
+    "plugin:@typescript-eslint/eslint-recommended", // Uses @typescript-eslint/eslint-plugin to adjust the rules set by `eslint:recommended` that are not compatible with TypeScript
+    "plugin:@typescript-eslint/recommended", // Uses the recommended rules from @typescript-eslint/eslint-plugin
+    "prettier/@typescript-eslint", // Uses eslint-config-prettier to disable rules that are unnecessary or might conflict with Prettier
+    "plugin:prettier/recommended" // Enables both eslint-plugin-prettier and extends the rules from eslint-config-prettier. Will report Prettier errors to ESLint.
+    // Only special rules should be added after `plugin:prettier/recommended`.
+    // See the list of special rules:
+    //   - https://github.com/prettier/eslint-config-prettier/blob/dcca556e260af985ece4d9af92531fa649268f80/README.md#special-rules
+  ]
 }
