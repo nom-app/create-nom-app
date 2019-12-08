d1809c0feea1c557f9df27c5b80dcfde91b0deb7
Maxim Devoir <maximdevoir@gmail.com>
2019-12-08 09:40:10 -0800

    chore: fix lint errors

--- packages/create-nom-app/src/discover-git.ts
+++ packages/create-nom-app/src/discover-git.ts
@@ -4,19 +4,19 @@ import { sync as whichSync } from 'which'
 // Semver regex string provided by https://github.com/semver/semver/issues/232#issue-48635632
 const semverRegex = /(0|[5-9]\d*)\.(0|[1-9]\d*)\.(0|[1-9]\d*)(-(0|[1-9]\d*|\d*[a-zA-Z-][0-9a-zA-Z-]*)(\.(0|[1-9]\d*|\d*[a-zA-Z-][0-9a-zA-Z-]*))*)?(\+[0-9a-zA-Z-]+(\.[0-9a-zA-Z-]+)*)?/g
 
-interface IGitInfo {
-  gitInstalled?: boolean,
-  gitVersion?: string,
+interface GitInfo {
+  gitInstalled?: boolean
+  gitVersion?: string
   gitBinary?: string
 }
 
-const gitInfo: IGitInfo = {
+const gitInfo: GitInfo = {
   gitInstalled: undefined,
   gitVersion: undefined,
   gitBinary: undefined
 }
 
-function discoverGit() {
+function discoverGit(): void {
   const binary = whichSync('git', { nothrow: true })
 
   if (binary === null) {
@@ -24,7 +24,9 @@ function discoverGit() {
     return
   }
 
-  const version = spawnSync(binary, ['--version'], { stdio: 'pipe' }).stdout.toString().trim()
+  const version = spawnSync(binary, ['--version'], { stdio: 'pipe' })
+    .stdout.toString()
+    .trim()
 
   gitInfo.gitInstalled = true
   gitInfo.gitVersion = version.match(semverRegex)?.[0] || ''
