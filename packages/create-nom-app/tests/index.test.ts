5f61e326d868e4d0cac1bd16e260274a05e4a785
Maxim Devoir <maximdevoir@gmail.com>
2019-12-09 13:50:16 -0800

    chore: add test for CLI help

--- packages/create-nom-app/tests/index.test.ts
+++ packages/create-nom-app/tests/index.test.ts
@@ -1,16 +1,19 @@
 /* eslint no-empty: "warn"*/
-import x from '../src/index'
+
+import CNA from '../src/index'
 
 describe('Create Nom App CLI', () => {
   let exitCode: null | number = null
   let spyOnLog: jest.SpyInstance
   let spyOnErr: jest.SpyInstance
   let spyOnExit: jest.SpyInstance
+  let spyOnStdOut: jest.SpyInstance
 
   beforeEach(() => {
     exitCode = null
     spyOnLog = jest.spyOn(console, 'log').mockImplementation()
     spyOnErr = jest.spyOn(console, 'error').mockImplementation()
+    spyOnStdOut = jest.spyOn(process.stdout, 'write').mockImplementation()
     spyOnExit = jest.spyOn(process, 'exit').mockImplementation(x => {
       exitCode = x || null
       throw new Error(`Mock process.exit()`)
@@ -22,7 +25,7 @@ describe('Create Nom App CLI', () => {
   })
   test('show usage instructions and exit with code 1 when no project name is supplied', () => {
     try {
-      void x([])
+      void CNA([])
     } catch (e) {
       // catches process.exit() - we don't need to do anything
     }
@@ -32,11 +35,31 @@ describe('Create Nom App CLI', () => {
     // expect(spyOnErr).toMatchSnapshot()
   })
 
+  describe('--help, -h', () => {
+    test('display help (-h)', () => {
+      try {
+        void CNA(['-h'])
+      } catch (e) {}
+      expect(exitCode).toStrictEqual(null)
+      expect(spyOnExit).toHaveBeenCalled()
+      expect(spyOnStdOut).toMatchSnapshot()
+    })
+
+    test('display help (--help)', () => {
+      try {
+        void CNA(['--help'])
+      } catch (e) {}
+      expect(exitCode).toStrictEqual(null)
+      expect(spyOnExit).toHaveBeenCalled()
+      expect(spyOnStdOut).toMatchSnapshot()
+    })
+  })
+
   describe('--info', () => {
     let returnValue: any = null
     test('gracefully return', () => {
       try {
-        returnValue = void x(['--info'])
+        returnValue = void CNA(['--info'])
       } catch (e) {}
       expect(returnValue).toStrictEqual(undefined)
       expect(exitCode).toStrictEqual(null)
@@ -45,7 +68,7 @@ describe('Create Nom App CLI', () => {
 
     test('display environment information', () => {
       try {
-        void x(['--info'])
+        void CNA(['--info'])
       } catch (e) {}
       expect(spyOnLog).toMatchSnapshot()
       expect(spyOnErr).toMatchSnapshot()
