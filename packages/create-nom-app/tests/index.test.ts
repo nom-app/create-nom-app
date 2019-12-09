3214b5c92f372633b4b46eae3fc87f51923f4efb
Maxim Devoir <maximdevoir@gmail.com>
2019-12-09 11:08:49 -0800

    chore(test): add testing for `create-nom-app --info`

--- packages/create-nom-app/tests/index.test.ts
+++ packages/create-nom-app/tests/index.test.ts
@@ -1,18 +1,26 @@
-/* eslint-disable @typescript-eslint/no-var-requires */
+/* eslint no-empty: "warn"*/
 import x from '../src/index'
 
 describe('Create Nom App CLI', () => {
   let exitCode: null | number = null
+  let spyOnLog: jest.SpyInstance
+  let spyOnErr: jest.SpyInstance
+  let spyOnExit: jest.SpyInstance
+
   beforeEach(() => {
     exitCode = null
-  })
-
-  test('show usage instructions and exit with code 1 when no project name is supplied', () => {
-    const spyOnExit = jest.spyOn(process, 'exit').mockImplementation(x => {
+    spyOnLog = jest.spyOn(console, 'log').mockImplementation()
+    spyOnErr = jest.spyOn(console, 'error').mockImplementation()
+    spyOnExit = jest.spyOn(process, 'exit').mockImplementation(x => {
       exitCode = x || null
       throw new Error(`Mock process.exit()`)
     })
-    const spyOnLog = jest.spyOn(console, 'log').mockImplementation()
+  })
+
+  afterEach(() => {
+    jest.clearAllMocks()
+  })
+  test('show usage instructions and exit with code 1 when no project name is supplied', () => {
     try {
       void x([])
     } catch (e) {
@@ -21,5 +29,26 @@ describe('Create Nom App CLI', () => {
     expect(exitCode).toStrictEqual(1)
     expect(spyOnExit).toHaveBeenCalledTimes(1)
     expect(spyOnLog).toMatchSnapshot()
+    // expect(spyOnErr).toMatchSnapshot()
+  })
+
+  describe('--info', () => {
+    let returnValue: any = null
+    test('gracefully return', () => {
+      try {
+        returnValue = void x(['--info'])
+      } catch (e) {}
+      expect(returnValue).toStrictEqual(undefined)
+      expect(exitCode).toStrictEqual(null)
+      expect(spyOnExit).toHaveBeenCalledTimes(0)
+    })
+
+    test('display environment information', () => {
+      try {
+        void x(['--info'])
+      } catch (e) {}
+      expect(spyOnLog).toMatchSnapshot()
+      expect(spyOnErr).toMatchSnapshot()
+    })
   })
 })
