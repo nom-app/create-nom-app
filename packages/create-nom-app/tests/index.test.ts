172184021d19d02f8bc9f7bcf3b6597ffa2fd769
Maxim Devoir <maximdevoir@gmail.com>
2019-12-09 09:54:21 -0800

    chore(test): add test for when no project name is supplied

--- packages/create-nom-app/tests/index.test.ts
+++ packages/create-nom-app/tests/index.test.ts
@@ -1,9 +1,25 @@
 /* eslint-disable @typescript-eslint/no-var-requires */
-describe('main', () => {
-  test('blank', () => {
-    // const spyOnExit = jest.spyOn(process, 'exit').mockImplementation()
-    // require('../src/index')(process.argv.slice(5))
-    // expect(spyOnExit).toHaveBeenCalledWith(0)
-    expect(true).toEqual(true)
+import x from '../src/index'
+
+describe('Create Nom App CLI', () => {
+  let exitCode: null | number = null
+  beforeEach(() => {
+    exitCode = null
+  })
+
+  test('show usage instructions and exit with code 1 when no project name is supplied', () => {
+    const spyOnExit = jest.spyOn(process, 'exit').mockImplementation(x => {
+      exitCode = x || null
+      throw new Error(`Mock process.exit()`)
+    })
+    const spyOnLog = jest.spyOn(console, 'log').mockImplementation()
+    try {
+      void x([])
+    } catch (e) {
+      // catches process.exit() - we don't need to do anything
+    }
+    expect(exitCode).toStrictEqual(1)
+    expect(spyOnExit).toHaveBeenCalledTimes(1)
+    expect(spyOnLog).toMatchSnapshot()
   })
 })
