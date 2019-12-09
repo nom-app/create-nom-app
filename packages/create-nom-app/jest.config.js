c08ef0e6b557826a6b8e0bc76ec32e5ef8cb67d3
Maxim Devoir <maximdevoir@gmail.com>
2019-12-09 13:47:41 -0800

    chore: remove `test` tsconfig file

--- packages/create-nom-app/jest.config.js
+++ packages/create-nom-app/jest.config.js
@@ -4,9 +4,13 @@ const baseConfig = require('../../jest.base')
 const config = Object.assign({}, baseConfig, {
   rootDir: __dirname,
   roots: ['<roodDir>', '<rootDir>/tests'],
-  setupFiles: ['./tests/setup.ts']
+  setupFiles: ['./tests/setup.ts'],
+  moduleNameMapper: {
+    'package.json': '<rootDir>/tests/__mocks__/package.ts'
+  }
 })
 
-config.globals['ts-jest'].tsConfig = 'tsconfig.test.json'
+config.globals['ts-jest'].tsConfig = 'tsconfig.json'
+config.globals['ts-jest'].diagnostics = { warnOnly: true }
 
 module.exports = config
