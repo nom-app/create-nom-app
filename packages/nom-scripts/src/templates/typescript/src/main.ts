8c0c43b0e235a1316a239c6dac5cd927b73ed152
Maxim Devoir <maximdevoir@gmail.com>
2020-01-15 15:33:35 -0800

    chore: add main.ts template

---
+++ packages/nom-scripts/src/templates/typescript/src/main.ts
@@ -0,0 +1,19 @@
+function add(number1: number, number2: number): number {
+  if (typeof number1 !== 'number' || typeof number2 !== 'number') {
+    throw Error('add() did not receive two numbers.')
+  }
+
+  return number1 + number2
+}
+
+function subtract(number1: number, number2: number): number {
+  if (typeof number1 !== 'number' || typeof number2 !== 'number') {
+    throw Error('subtract() did not receive two numbers.')
+  }
+
+  return number1 - number2
+}
+
+export default add
+
+export { add, subtract }
